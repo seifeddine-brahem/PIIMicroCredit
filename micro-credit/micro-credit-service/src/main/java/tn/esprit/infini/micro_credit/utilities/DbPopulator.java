e6787ed10e7bfccaa3c2a917870a3f0607ceb060
tarekchaaben <48210547+tarekchaaben@users.noreply.github.com>
2019-05-11 11:24:53 +0100

    manger update Signed-off-by: tarekchaaben <48210547+tarekchaaben@users.noreply.github.com>

--- micro-credit/micro-credit-service/src/main/java/tn/esprit/infini/micro_credit/utilities/DbPopulator.java
+++ micro-credit/micro-credit-service/src/main/java/tn/esprit/infini/micro_credit/utilities/DbPopulator.java
@@ -36,6 +36,7 @@ public class DbPopulator {
 	@PostConstruct
 	private void inigt() {
 		User user = new User("u", "u", "u@esprit.tn", "u", "u", "u1", Role.agent);
+		User user1 = new User("u1", "u1", "u1@esprit.tn", "u1", "u1", "u1", Role.agent);
 		User user2 = new User("u2", "u2", "u2@esprit.tn", "u2", "u2", "u1", Role.client);
 		User user3 = new User("u3", "u3", "u3@esprit.tn", "u3", "u3", "u1", Role.client);
 
@@ -43,9 +44,9 @@ public class DbPopulator {
 		Account account2 = new Account("12346", "1000", AccountType.DepositAccount);
 		Account account3 = new Account("12347", "1000", AccountType.CurrentAccount);
 
-		CardOffer cardOffer = new CardOffer("master-card","master","123456789", new Date(), 123);
-		CardOffer cardOffer2 = new CardOffer("e-dinar","e-dinar","12345678910", new Date(), 124);
-		CardOffer cardOffer3 = new CardOffer("post-card","post","12345678911", new Date(), 125);
+		CardOffer cardOffer = new CardOffer("master-card", "master-card", "123456789", new Date(), 123);
+		CardOffer cardOffer2 = new CardOffer("e-dinar", "e-dinar", "12345678910", new Date(), 124);
+		CardOffer cardOffer3 = new CardOffer("post-card", "post", "12345678911", new Date(), 125);
 
 		List<Account> accounts = new ArrayList<>();
 		accounts.add(account);
@@ -59,6 +60,7 @@ public class DbPopulator {
 		user3.linkAccounts(accounts2);
 
 		em.merge(user);
+		em.merge(user1);
 		em.merge(user2);
 		em.merge(user3);
 		em.merge(cardOffer);
